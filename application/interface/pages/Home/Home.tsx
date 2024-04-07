82bf9f35ba902c7ccfe177f01cc93c24445b3047
Arthur Schulle Konrad <arthurskonrad@gmail.com>
2024-04-07 18:03:21 -0300

    chore: setup shadcnui

---
+++ application/interface/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { TypographyH1 } from "interface/components/Typography/TypographyH1";
+
+function Home() {
+  return (
+    <div>
+      <TypographyH1>Home</TypographyH1>
+    </div>
+  );
+}
+
+export default Home;
